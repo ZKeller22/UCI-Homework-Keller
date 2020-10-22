fe928a64db09d0f58fd8eeac86cdcdff01fb3492
zane.keller22@gmail.com <zane.keller22@gmail.com>
2020-10-21 17:24:22 -0700

    Saving with error to image

--- python-api-challenge/VacationPy/api_keys.py
+++ python-api-challenge/VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "94c75c439e333f1e7e6baae2610da587"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBV9GbfnDQDTbZU2D892bYSYYXgNfh6ryk"
